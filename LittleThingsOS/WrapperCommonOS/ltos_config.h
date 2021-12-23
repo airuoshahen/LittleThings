4f43287cace6956f55b85351f2a14bd1d8ac2601
Li <Hansson.Li@Honeywell.com>
2021-12-23 08:37:29 +0800

    add configuration file of LittleThings OS

---
+++ LittleThingsOS/WrapperCommonOS/ltos_config.h
@@ -0,0 +1,19 @@
+/*
+ * @Author: Hansson Li
+ * @Date: 2021-12-22 15:18:13
+ * @LastEditTime: 2021-12-22 16:07:26
+ * @LastEditors: Hansson Li
+ * @Description: used to define some configuration MICRO
+ * MIT License
+ * 
+ * Copyright (c) 2021 一页诗词郎
+ * 
+ */
+
+
+#ifndef __LTOS_CONFIG_H__
+#define __LTOS_CONFIG_H__
+
+#define USE_FREERTOS // currently the platform just support FreeRTOS
+
+#endif
