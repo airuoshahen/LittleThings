5d564bd934846cd6daedff1476972be2629541bb
Li <Hansson.Li@Honeywell.com>
2021-12-23 14:27:43 +0800

    implement some api about task and schdule

--- LittleThingsOS/WrapperCommonOS/ltos_task.c
+++ LittleThingsOS/WrapperCommonOS/ltos_task.c
@@ -1,7 +1,7 @@
 /*
  * @Author: Hansson Li
  * @Date: 2021-12-22 14:54:15
- * @LastEditTime: 2021-12-22 16:31:21
+ * @LastEditTime: 2021-12-23 14:21:02
  * @LastEditors: Hansson Li
  * @Description: 
  * MIT License
@@ -34,29 +34,427 @@
  *                               神兽保佑            永无BUG
  */
 
-#include "ltos_config.h"
 #include "ltos_task.h"
-#ifdef USE_FREERTOS
-#include "FreeRTOS.h"
-#include "task.h"
-#endif
 
-#ifndef USE_FREERTOS
-#define ltos_task_err() do{}while(1);
-#endif
+#define UNSUPPORT_OS_RET_NULL   return
+#define UNSUPPORT_OS_RET_ERR    do{return LTOS_TASK_UNSUPPORT_OS;}while(0)
 
 /**
  * @description: task create function
- * @param {*}
+ * @param {lt_task_handle_ptr -- pointer of task handle}
  * @return LTOS_TASK_OK -- create successfully
  *         LTOS_TASK_ERR -- create failed
  */
-ltos_task_ret_e ltos_task_create()
+ltos_task_ret_e ltos_task_create(ltos_task_handle_t *lt_task_handle_ptr, const ltos_s8 * const lt_task_name,
+    ltos_task_function_t lt_task_function, void * const lt_task_input_parameter, ltos_u16 lt_task_stack_size,
+    ltos_u16 lt_task_priority)
 {
     #ifdef USE_FREERTOS
-    if (xTaskCreate())
+    if (xTaskCreate(lt_task_function, lt_task_name, lt_task_stack_size, lt_task_input_parameter, lt_task_priority,
+        lt_task_handle_ptr) == pdPASS)
+    {
+        return LTOS_TASK_OK;
+    }
+    else
+    {
+        return LTOS_TASK_ERR;
+    }
     #else
-    ltos_task_err();
-    return LTOS_TASK_ERR;
+    UNSUPPORT_OS_RET_ERR;
     #endif
 }
+
+/**
+ * @description: task delay function
+ * @param {ltos_tick_t} delay_ticks -- ticks need to delay(1ms)
+ * @return {*}
+ */
+void ltos_task_delay(ltos_tick_t delay_ticks)
+{
+    #ifdef USE_FREERTOS
+    vTaskDelay(delay_ticks);
+    #else
+    UNSUPPORT_OS_RET_NULL;
+    #endif
+}
+
+/**
+ * @description: delete the unused task
+ * @param {ltos_task_handle_t} lt_task_handle -- handle want to delete
+ * @return {*}
+ */
+void ltos_task_delete(ltos_task_handle_t lt_task_handle)
+{
+    #ifdef USE_FREERTOS
+    vTaskDelete(lt_task_handle);
+    #else
+    UNSUPPORT_OS_RET_NULL;
+    #endif
+}
+
+/**
+ * @description: disable interrupts (it is effected by the configMAX_SYSCALL_INTERRUPT_PRIORITY paramter in configuration file)
+ * @param {*}
+ * @return {*}
+ */
+void ltos_task_disable_interrupts(void)
+{
+    #ifdef USE_FREERTOS
+    taskDISABLE_INTERRUPTS();
+    #else
+    UNSUPPORT_OS_RET_NULL;
+    #endif
+}
+
+/**
+ * @description: enable interrupts
+ * @param {*}
+ * @return {*}
+ */
+void ltos_task_enable_interrupts(void)
+{
+    #ifdef USE_FREERTOS
+    taskENABLE_INTERRUPTS();
+    #else
+    UNSUPPORT_OS_RET_NULL;
+    #endif
+}
+
+/**
+ * @description: it must be used in pair with ltos_task_enter_critical, after it's called, critical section will be entered
+ *               with simply disabling interrupts, it can't be called from an interrupt service routine
+ * @param {*}
+ * @return {*}
+ */
+void ltos_task_enter_critical(void)
+{
+    #ifdef USE_FREERTOS
+    taskENTER_CRITICAL();
+    #else
+    UNSUPPORT_OS_RET_NULL;
+    #endif
+}
+
+/**
+ * @description: exit critical section, same note with ltos_task_enter_critical function
+ * @param {*}
+ * @return {*}
+ */
+void ltos_task_exit_critical(void)
+{
+    #ifdef USE_FREERTOS
+    taskEXIT_CRITICAL();
+    #else
+    UNSUPPORT_OS_RET_NULL;
+    #endif
+}
+
+/**
+ * @description: can be used in an interrupt service routine
+ * @param {*}
+ * @return {*}
+ */
+void ltos_task_enter_critical_from_isr(void)
+{
+    #ifdef USE_FREERTOS
+    taskENTER_CRITICAL_FROM_ISR();
+    #else
+    UNSUPPORT_OS_RET_NULL;
+    #endif
+}
+
+/**
+ * @description: 
+ * @param {*}
+ * @return {*}
+ */
+void ltos_task_exit_critical_from_isr(void)
+{
+    #ifdef USE_FREERTOS
+    taskEXIT_CRITICAL_FROM_ISR();
+    #else
+    UNSUPPORT_OS_RET_NULL;
+    #endif
+}
+
+/**
+ * @description: used to communication between tasks
+ * @param {ltos_task_handle_t} lt_task_handle
+ * @return {*}
+ */
+ltos_task_ret_e ltos_task_notify(ltos_task_handle_t lt_task_handle)
+{
+    #ifdef USE_FREERTOS
+    if (xTaskNotify(lt_task_handle, 0, eNoAction) == pdPASS)
+    {
+        return LTOS_TASK_OK;
+    }
+    else
+    {
+        return LTOS_TASK_ERR;
+    }
+    #else
+    UNSUPPORT_OS_RET_ERR;
+    #endif
+}
+
+/**
+ * @description: the task's notification value is incremented by one
+ * @param {ltos_task_handle_t} lt_task_handle
+ * @return {*}
+ */
+ltos_task_ret_e ltos_task_notify_increment(ltos_task_handle_t lt_task_handle)
+{
+    #ifdef USE_FREERTOS
+    if (xTaskNotify(lt_task_handle, 0, eIncrement) == pdPASS)
+    {
+        return LTOS_TASK_OK;
+    }
+    else
+    {
+        return LTOS_TASK_ERR;
+    }
+    #else
+    UNSUPPORT_OS_RET_ERR;
+    #endif
+}
+
+/**
+ * @description: the task's notification value and bit_value is ORed
+ * @param {ltos_task_handle_t} lt_task_handle
+ * @param {ltos_u32} bit_value -- 1 << 8
+ * @return {*}
+ */
+ltos_task_ret_e ltos_task_notify_setbits(ltos_task_handle_t lt_task_handle, ltos_u32 bit_value)
+{
+    #ifdef USE_FREERTOS
+    if (xTaskNotify(lt_task_handle, bit_value, eSetBits) == pdPASS)
+    {
+        return LTOS_TASK_OK;
+    }
+    else
+    {
+        return LTOS_TASK_ERR;
+    }
+    #else
+    UNSUPPORT_OS_RET_ERR;
+    #endif
+}
+
+/**
+ * @description: the task's notification value will be set set_value, if task has unhandled notification, 
+ *               this function will return ERROR
+ * @param {ltos_task_handle_t} lt_task_handle
+ * @param {ltos_u32} set_value
+ * @return {*}
+ */
+ltos_task_ret_e ltos_task_notify_setvalue(ltos_task_handle_t lt_task_handle, ltos_u32 set_value)
+{
+    #ifdef USE_FREERTOS
+    if (xTaskNotify(lt_task_handle, set_value, eSetValueWithoutOverwrite) == pdPASS)
+    {
+        return LTOS_TASK_OK;
+    }
+    else
+    {
+        return LTOS_TASK_ERR;
+    }
+    #else
+    UNSUPPORT_OS_RET_ERR;
+    #endif
+}
+
+
+/**
+ * @description: used to communication between tasks
+ * @param {ltos_task_handle_t} lt_task_handle
+ * @return {*}
+ */
+ltos_task_ret_e ltos_task_notify_from_isr(ltos_task_handle_t lt_task_handle)
+{
+    #ifdef USE_FREERTOS
+    BaseType_t lt_higher_priority_task_woken = pdFALSE;
+    
+    if (xTaskNotifyFromISR(lt_task_handle, 0, eNoAction, &lt_higher_priority_task_woken) == pdPASS)
+    {
+        portYIELD_FROM_ISR(lt_higher_priority_task_woken);
+        return LTOS_TASK_OK;
+    }
+    else
+    {
+        portYIELD_FROM_ISR(lt_higher_priority_task_woken);
+        return LTOS_TASK_ERR;
+    }
+    #else
+    UNSUPPORT_OS_RET_ERR;
+    #endif
+}
+
+/**
+ * @description: the task's notification value is incremented by one
+ * @param {ltos_task_handle_t} lt_task_handle
+ * @return {*}
+ */
+ltos_task_ret_e ltos_task_notify_increment_from_isr(ltos_task_handle_t lt_task_handle)
+{
+    #ifdef USE_FREERTOS
+    BaseType_t lt_higher_priority_task_woken = pdFALSE;
+
+    if (xTaskNotifyFromISR(lt_task_handle, 0, eIncrement, &lt_higher_priority_task_woken) == pdPASS)
+    {
+        portYIELD_FROM_ISR(lt_higher_priority_task_woken);
+        return LTOS_TASK_OK;
+    }
+    else
+    {
+        portYIELD_FROM_ISR(lt_higher_priority_task_woken);
+        return LTOS_TASK_ERR;
+    }
+    #else
+    UNSUPPORT_OS_RET_ERR;
+    #endif
+}
+
+/**
+ * @description: the task's notification value and bit_value is ORed
+ * @param {ltos_task_handle_t} lt_task_handle
+ * @param {ltos_u32} bit_value -- 1 << 8
+ * @return {*}
+ */
+ltos_task_ret_e ltos_task_notify_setbits_from_isr(ltos_task_handle_t lt_task_handle, ltos_u32 bit_value)
+{
+    #ifdef USE_FREERTOS
+    BaseType_t lt_higher_priority_task_woken = pdFALSE;
+
+    if (xTaskNotifyFromISR(lt_task_handle, bit_value, eSetBits, &lt_higher_priority_task_woken) == pdPASS)
+    {
+        portYIELD_FROM_ISR(lt_higher_priority_task_woken);
+        return LTOS_TASK_OK;
+    }
+    else
+    {
+        portYIELD_FROM_ISR(lt_higher_priority_task_woken);
+        return LTOS_TASK_ERR;
+    }
+    #else
+    UNSUPPORT_OS_RET_ERR;
+    #endif
+}
+
+/**
+ * @description: the task's notification value will be set set_value, if task has unhandled notification, 
+ *               this function will return ERROR
+ * @param {ltos_task_handle_t} lt_task_handle
+ * @param {ltos_u32} set_value
+ * @return {*}
+ */
+ltos_task_ret_e ltos_task_notify_setvalue_from_isr(ltos_task_handle_t lt_task_handle, ltos_u32 set_value)
+{
+    #ifdef USE_FREERTOS
+    BaseType_t lt_higher_priority_task_woken = pdFALSE;
+
+    if (xTaskNotifyFromISR(lt_task_handle, set_value, eSetValueWithoutOverwrite, &lt_higher_priority_task_woken) == pdPASS)
+    {
+        portYIELD_FROM_ISR(lt_higher_priority_task_woken);
+        return LTOS_TASK_OK;
+    }
+    else
+    {
+        portYIELD_FROM_ISR(lt_higher_priority_task_woken);
+        return LTOS_TASK_ERR;
+    }
+    #else
+    UNSUPPORT_OS_RET_ERR;
+    #endif
+}
+
+
+/**
+ * @description: 
+ * @param {ltos_task_handle_t} lt_task_handle
+ * @param {ltos_u32} *lt_previous_notify_value_ptr -- used to get the previous notify value of this task
+ * @return {*}
+ */
+ltos_task_ret_e ltos_task_notify_with_query(ltos_task_handle_t lt_task_handle, ltos_u32 *lt_previous_notify_value_ptr)
+{
+    #ifdef USE_FREERTOS
+    if (xTaskNotifyAndQuery(lt_task_handle, 0, eNoAction, lt_previous_notify_value_ptr) == pdPASS)
+    {
+        return LTOS_TASK_OK;
+    }
+    else
+    {
+        return LTOS_TASK_ERR;
+    }
+    #else
+    UNSUPPORT_OS_RET_ERR;
+    #endif
+}
+
+/**
+ * @description: the task's notification value is incremented by one
+ * @param {ltos_task_handle_t} lt_task_handle
+ * @return {*}
+ */
+ltos_task_ret_e ltos_task_notify_increment_with_query(ltos_task_handle_t lt_task_handle, ltos_u32 *lt_previous_notify_value_ptr)
+{
+    #ifdef USE_FREERTOS
+    if (xTaskNotifyAndQuery(lt_task_handle, 0, eIncrement, lt_previous_notify_value_ptr) == pdPASS)
+    {
+        return LTOS_TASK_OK;
+    }
+    else
+    {
+        return LTOS_TASK_ERR;
+    }
+    #else
+    UNSUPPORT_OS_RET_ERR;
+    #endif
+}
+
+/**
+ * @description: the task's notification value and bit_value is ORed
+ * @param {ltos_task_handle_t} lt_task_handle
+ * @param {ltos_u32} bit_value -- 1 << 8
+ * @return {*}
+ */
+ltos_task_ret_e ltos_task_notify_setbits_with_query(ltos_task_handle_t lt_task_handle, ltos_u32 bit_value, ltos_u32 *lt_previous_notify_value_ptr)
+{
+    #ifdef USE_FREERTOS
+    if (xTaskNotifyAndQuery(lt_task_handle, bit_value, eSetBits, lt_previous_notify_value_ptr) == pdPASS)
+    {
+        return LTOS_TASK_OK;
+    }
+    else
+    {
+        return LTOS_TASK_ERR;
+    }
+    #else
+    UNSUPPORT_OS_RET_ERR;
+    #endif
+}
+
+/**
+ * @description: the task's notification value will be set set_value, if task has unhandled notification, 
+ *               this function will return ERROR
+ * @param {ltos_task_handle_t} lt_task_handle
+ * @param {ltos_u32} set_value
+ * @return {*}
+ */
+ltos_task_ret_e ltos_task_notify_setvalue_with_query(ltos_task_handle_t lt_task_handle, ltos_u32 set_value, ltos_u32 *lt_previous_notify_value_ptr)
+{
+    #ifdef USE_FREERTOS
+    if (xTaskNotifyAndQuery(lt_task_handle, set_value, eSetValueWithoutOverwrite, lt_previous_notify_value_ptr) == pdPASS)
+    {
+        return LTOS_TASK_OK;
+    }
+    else
+    {
+        return LTOS_TASK_ERR;
+    }
+    #else
+    UNSUPPORT_OS_RET_ERR;
+    #endif
+}
+
+// TODO: implement xTaskNotifyGive()
--- LittleThingsOS/WrapperCommonOS/ltos_task.h
+++ LittleThingsOS/WrapperCommonOS/ltos_task.h
@@ -1,7 +1,7 @@
 /*
  * @Author: Hansson Li
  * @Date: 2021-12-22 16:08:39
- * @LastEditTime: 2021-12-23 09:01:50
+ * @LastEditTime: 2021-12-23 14:24:58
  * @LastEditors: Hansson Li
  * @Description: LittleThings OS task header file
  * MIT License
@@ -18,7 +18,33 @@
 typedef enum
 {
     LTOS_TASK_OK = 0,
-    LTOS_TASK_ERR
+    LTOS_TASK_ERR,
+    LTOS_TASK_UNSUPPORT_OS,
 }ltos_task_ret_e;
+
+ltos_task_ret_e ltos_task_create(ltos_task_handle_t *lt_task_handle_ptr, const ltos_s8 * const lt_task_name,
+    ltos_task_function_t lt_task_function, void * const lt_task_input_parameter, ltos_u16 lt_task_stack_size,
+    ltos_u16 lt_task_priority);
+void ltos_task_delay(ltos_tick_t delay_ticks);
+void ltos_task_delete(ltos_task_handle_t lt_task_handle);
+void ltos_task_disable_interrupts(void);
+void ltos_task_enable_interrupts(void);
+void ltos_task_enter_critical(void);
+void ltos_task_exit_critical(void);
+void ltos_task_enter_critical_from_isr(void);
+void ltos_task_exit_critical_from_isr(void);
+ltos_task_ret_e ltos_task_notify(ltos_task_handle_t lt_task_handle);
+ltos_task_ret_e ltos_task_notify_increment(ltos_task_handle_t lt_task_handle);
+ltos_task_ret_e ltos_task_notify_setbits(ltos_task_handle_t lt_task_handle, ltos_u32 bit_value);
+ltos_task_ret_e ltos_task_notify_setvalue(ltos_task_handle_t lt_task_handle, ltos_u32 set_value);
+ltos_task_ret_e ltos_task_notify_from_isr(ltos_task_handle_t lt_task_handle);
+ltos_task_ret_e ltos_task_notify_increment_from_isr(ltos_task_handle_t lt_task_handle);
+ltos_task_ret_e ltos_task_notify_setbits_from_isr(ltos_task_handle_t lt_task_handle, ltos_u32 bit_value);
+ltos_task_ret_e ltos_task_notify_setvalue_from_isr(ltos_task_handle_t lt_task_handle, ltos_u32 set_value);
+ltos_task_ret_e ltos_task_notify_with_query(ltos_task_handle_t lt_task_handle, ltos_u32 *lt_previous_notify_value_ptr);
+ltos_task_ret_e ltos_task_notify_increment_with_query(ltos_task_handle_t lt_task_handle, ltos_u32 *lt_previous_notify_value_ptr);
+ltos_task_ret_e ltos_task_notify_setbits_with_query(ltos_task_handle_t lt_task_handle, ltos_u32 bit_value, ltos_u32 *lt_previous_notify_value_ptr);
+ltos_task_ret_e ltos_task_notify_setvalue_with_query(ltos_task_handle_t lt_task_handle, ltos_u32 set_value, ltos_u32 *lt_previous_notify_value_ptr);
+
 
 #endif
